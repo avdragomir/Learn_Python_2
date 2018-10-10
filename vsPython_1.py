52b3e39ab171e308f9273cfa348794ef67ea39f8
avdragomir <43769757+avdragomir@users.noreply.github.com>
2018-10-10 16:17:55 -0600

    chap_2

--- vsPython_1.py
+++ vsPython_1.py
@@ -1,2 +1,9 @@
 msg = "What's up?"
 print(msg)
+def least_diff(a,b,c):
+    diff1 = abs(a-b)
+    diff2 = abs(b-c)
+    diff3 = abs(a-c)
+    return min(diff1,diff2,diff3)
+x = least_diff(5,4,1)
+print(x)
